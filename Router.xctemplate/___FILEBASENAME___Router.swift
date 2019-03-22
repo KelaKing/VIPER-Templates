c562c7f3b94d71e7b1659231484ff4ff5a678c97
KelaKing <jxwywxy@icloud.com>
2019-03-22 18:24:13 +0800

    Update Router.xctemplate

--- Router.xctemplate/___FILEBASENAME___Router.swift
+++ Router.xctemplate/___FILEBASENAME___Router.swift
@@ -4,4 +4,14 @@ import Foundation
 
 class ___FILEBASENAMEASIDENTIFIER___: ___FILEBASENAMEASIDENTIFIER___Input {
 
+    static func createModule() -> ___VARIABLE_productName:identifier___ViewController {
+        let viewController = ___VARIABLE_productName:identifier___ViewController()
+        let presenter = ___VARIABLE_productName:identifier___Presenter()
+        let interactor = ___VARIABLE_productName:identifier___Interactor()
+        viewController.output = presenter
+        presenter.view = viewController
+        presenter.interactor = interactor
+        interactor.output = presenter
+        return viewController
+    }
 }
--- Router.xctemplate/___FILEBASENAME___RouterInput.swift
+++ Router.xctemplate/___FILEBASENAME___RouterInput.swift
@@ -3,5 +3,6 @@
 import Foundation
 
 protocol ___FILEBASENAMEASIDENTIFIER___ {
-    
+
+    static func createModule() -> ___VARIABLE_productName:identifier___ViewController
 }
